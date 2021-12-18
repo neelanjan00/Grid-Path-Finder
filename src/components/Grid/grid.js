b2dba21b45295d524224192746afee603ebc7d5f
neelanjan00 <neelanjan@chaosnative.com>
2021-12-18 12:39:02 +0530

    added logic for selecting starting and ending positions
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- src/components/Grid/grid.js
+++ src/components/Grid/grid.js
@@ -1,5 +1,37 @@
 // MakeGrid renders a table of dimensions rows x cols, as per the values in grid 
-const MakeGrid = ({rows, cols, grid}) => {
+const MakeGrid = ({
+  startingPosition, setStartingPosition,
+  endingPosition, setEndingPosition,
+  rows, cols, 
+  grid
+}) => {
+
+  // cellClickHandler assigns the starting and ending positions
+  const cellClickHandler = (i, j) => {
+
+    if(startingPosition.length === 0 && grid[i][j] === 0) {
+      setStartingPosition([i, j])
+    } else if(i === startingPosition[0] && j === startingPosition[1] && endingPosition.length === 0) {
+      setStartingPosition([])
+    } else if(endingPosition.length === 0 && grid[i][j] === 0) {
+      setEndingPosition([i, j])
+    } else if(i === endingPosition[0] && j === endingPosition[1]) {
+      setEndingPosition([])
+    }
+  }
+
+  // getCellColor returns the color of a cell, given its coordinates
+  const getCellColor = (i, j) => {
+
+    if(startingPosition.length !== 0 && i === startingPosition[0] && j === startingPosition[1]) {
+      return "green"
+    } else if(endingPosition.length !== 0 && i === endingPosition[0] && j === endingPosition[1]) {
+      return "red"
+    }
+
+    return grid[i][j] === 0 ? "lightgrey" : "black"
+  }
+
   return (
     <table>
       <tbody>
@@ -7,10 +39,11 @@ const MakeGrid = ({rows, cols, grid}) => {
           <tr key={i}>
             {Array(cols).fill(1).map((_, j) => (
               <td key={j} 
+                  onClick={() => {cellClickHandler(i, j)}}
                   style={{ 
                     width: "20px", 
                     height: "20px", 
-                    backgroundColor: grid[i][j] === 0 ? "lightgrey" : "black"
+                    backgroundColor: getCellColor(i, j)
                   }}></td>
             ))}
           </tr>
--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -33,13 +33,50 @@ const Home = () => {
   const [startingPosition, setStartingPosition] = useState([])
   const [endingPosition, setEndingPosition] = useState([])
 
-  const [grid, setGrid] = useState(getRandomArray(35, 70))
+  const [grid, setGrid] = useState(getRandomArray(25, 50))
+
+  const findPath = () => {
+    
+  }
 
   return (
     <div>
-      <MakeGrid 
-        rows={35} cols={70}
-        grid = {grid} />
+
+      <h1 style={{ textAlign: 'center', fontWeight: 700 }}>Path Finder</h1>
+
+      <div style={{
+        display: 'flex',  
+        justifyContent:'center', 
+        alignItems:'center'}}>
+        <MakeGrid 
+          startingPosition = {startingPosition} setStartingPosition={setStartingPosition}
+          endingPosition = {endingPosition} setEndingPosition={setEndingPosition}
+          rows={25} cols={50}
+          grid = {grid} />
+      </div>
+
+      <div style={{
+        display: 'flex', 
+        justifyContent:'center', 
+        alignItems:'center', 
+        marginTop: '30px'}}>
+        <button
+          onClick={() => findPath()} 
+          style={{
+            padding: '10px',
+            paddingLeft: '13px',
+            paddingRight: '13px', 
+            borderRadius: '3px',
+            outline: '0',
+            border: '0',
+            cursor: 'pointer',
+            backgroundColor: 'lightgrey',
+            display: startingPosition.length !== 0 && endingPosition.length !== 0 ? 'inherit' : 'none'
+          }}>
+          Find Path
+        </button>
+      </div>
+
     </div>
   )
 }
