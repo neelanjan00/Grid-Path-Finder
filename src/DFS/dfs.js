00f489f6a0d4df60f3b3e67e8a6e62068cd9aa56
neelanjan00 <neelanjan@chaosnative.com>
2021-12-19 17:04:24 +0530

    added dfs search
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- src/DFS/dfs.js
+++ src/DFS/dfs.js
@@ -1,7 +1,7 @@
 import { GetArray } from "../utility/array";
 
 /**
- * BFS finds the shortest path between the source and the destination using BFS. It returns the parent and visited array when the source vertex is found.
+ * 
  * @param {number[]} start starting position coordinates
  * @param {number[]} end ending position coordinates
  * @param {number} rows number of rows in the graph 2D array
@@ -9,16 +9,18 @@ import { GetArray } from "../utility/array";
  * @param {number[][]} graph graph 2D array
  * @returns {[number[][][], number[][]]} the parent array and the visited array
  */
-const BFS = (start, end, rows, cols, graph) => {
+const DFS = (start, end, rows, cols, graph) => {
+
   let parent = GetArray(rows, cols, false);
   let visited = GetArray(rows, cols, false);
 
-  let queue = [start];
+  let stack = [start];
   visited[start[0]][start[1]] = 1;
   parent[start[0]][start[1]] = [start[0], start[1]];
 
-  while (queue.length > 0) {
-    let node = queue.shift();
+  while (stack.length > 0) {
+    
+    let node = stack.pop()
 
     if (node[0] === end[0] && node[1] === end[1]) {
       return [parent, visited];
@@ -29,32 +31,32 @@ const BFS = (start, end, rows, cols, graph) => {
 
     //top
     if (x !== 0 && graph[x - 1][y] === 0 && visited[x - 1][y] === 0) {
-      queue.push([x - 1, y]);
+      stack.push([x - 1, y]);
       visited[x - 1][y] = 1;
       parent[x - 1][y] = node;
     }
 
     //bottom
     if (x !== rows - 1 && graph[x + 1][y] === 0 && visited[x + 1][y] === 0) {
-      queue.push([x + 1, y]);
+      stack.push([x + 1, y]);
       visited[x + 1][y] = 1;
       parent[x + 1][y] = node;
     }
 
     //left
     if (y !== 0 && graph[x][y - 1] === 0 && visited[x][y - 1] === 0) {
-      queue.push([x, y - 1]);
+      stack.push([x, y - 1]);
       visited[x][y - 1] = 1;
       parent[x][y - 1] = node;
     }
 
     //right
     if (y !== cols - 1 && graph[x][y + 1] === 0 && visited[x][y + 1] === 0) {
-      queue.push([x, y + 1]);
+      stack.push([x, y + 1]);
       visited[x][y + 1] = 1;
       parent[x][y + 1] = node;
     }
   }
 };
 
-export { BFS };
+export { DFS };
--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GetArray } from "../../utility/array";
 import { BFS } from "../../BFS/bfs";
+import { DFS } from "../../DFS/dfs";
 import { ReconstructPath } from "../../utility/path";
 import MakeGrid from "../../components/Grid/grid";
 
@@ -13,21 +14,30 @@ const Home = () => {
 
   const [grid, setGrid] = useState(GetArray(rows, cols, true));
 
+  const searchAlgorithms = ["BFS", "DFS"];
+
   /**
    * tracePath finds a path between the source and the destination and traces it on the grid.
    */
-  const tracePath = () => {
-    let obj = BFS(
-      startingPosition,
-      endingPosition,
-      rows,
-      cols,
-      grid
-    );
+  const tracePath = (algorithm) => {
+    let obj;
 
-    if(typeof obj === 'undefined') {
-      alert(`No path exists between the starting and ending position.`)
-      return
+    switch (algorithm) {
+      case "BFS":
+        obj = BFS(startingPosition, endingPosition, rows, cols, grid);
+        break;
+
+      case "DFS":
+        obj = DFS(startingPosition, endingPosition, rows, cols, grid);
+        break;
+
+      default:
+        return;
+    }
+
+    if (typeof obj === "undefined") {
+      alert(`No path exists between the starting and ending position.`);
+      return;
     }
 
     let parent = obj[0];
@@ -84,25 +94,30 @@ const Home = () => {
           marginTop: "30px",
         }}
       >
-        <button
-          onClick={() => tracePath()}
-          style={{
-            padding: "10px",
-            paddingLeft: "13px",
-            paddingRight: "13px",
-            borderRadius: "3px",
-            outline: "0",
-            border: "0",
-            cursor: "pointer",
-            backgroundColor: "lightgrey",
-            display:
-              startingPosition.length !== 0 && endingPosition.length !== 0
-                ? "inherit"
-                : "none",
-          }}
-        >
-          Find Path
-        </button>
+        {searchAlgorithms.map((algorithm) => {
+          return (
+            <button
+              onClick={() => tracePath(algorithm)}
+              style={{
+                padding: "10px",
+                marginLeft: "10px",
+                paddingLeft: "13px",
+                paddingRight: "13px",
+                borderRadius: "3px",
+                outline: "0",
+                border: "0",
+                cursor: "pointer",
+                backgroundColor: "lightgrey",
+                display:
+                  startingPosition.length !== 0 && endingPosition.length !== 0
+                    ? "inherit"
+                    : "none",
+              }}
+            >
+              {algorithm}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
